7b8b9301b569c86881ace8bb087d9472f41238a2
Manish Goyal <manish.goyal@mobilelive.ca>
2019-01-08 13:24:22 -0500

    remove worker

--- example/main.js
+++ example/main.js
@@ -14,9 +14,6 @@ postal.instanceId( "parent" );
 postal.fedx.addFilter( [
 	{ channel: 'postal', topic: '#', direction: 'out' },
 	{ channel: 'iframez', topic: '#', direction: 'out' },
-	{ channel: 'webworker1', topic: '#', direction: 'out' },
-	{ channel: 'webworker2', topic: '#', direction: 'out' },
-	{ channel: 'webworker', topic: '#', direction: 'both' },
 	{ channel: 'parentz', topic: '#', direction: 'in' },
 	{ channel: 'reqres', topic: '#', direction: 'both' },
 	{ channel: 'postal.request-response', topic: '#', direction: 'both' }
@@ -26,43 +23,7 @@ postal.addWireTap( function( d, e ) {
 } );
 
 $( function() {
-
-	if ( false ) {
-		/*
-		window.werker1 = new Worker( 'worker.js' );
-		window.werker2 = new Worker( 'worker2.js' );
-		werker1.postMessage( "" );
-		werker2.postMessage( "" );
-		postal.fedx.signalReady( { xframe: [ { target: werker1 } ] }, function() {} );
-		postal.fedx.signalReady( { xframe: [ { target: werker2 } ] }, function() {} );
-		//postal.fedx.transports.xframe.listenToWorker( werker2 );
-
-		$( "#worker1" ).on( 'click', function() {
-			postal.publish( {
-				channel: "webworker1",
-				topic: "hit.me.baby.one.more.time"
-			} );
-		} );
-
-		$( "#worker2" ).on( 'click', function() {
-			postal.publish( {
-				channel: "webworker2",
-				topic: "hit.me.baby.one.more.time"
-			} );
-		} );
-
-		postal.subscribe( {
-			channel: "webworker",
-			topic: "#",
-			callback: function( d, e ) {
-				$( "#msgs" ).append( "<div><pre>" + JSON.stringify( e, null, 4 ) + "</pre></div>" );
-			}
-		} ); */
-	} else {
-		$( "#worker1" ).remove();
-		$( "#worker2" ).remove();
-	}
-
+	
 	postal.subscribe( {
 		channel: "parentz",
 		topic: "#",
